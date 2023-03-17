f50e2ca8c5b254c8d635c0727e3ffad0ce92a1d5
danieljuma5 <jumadaniel275@gmail.com>
2023-03-18 00:37:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+// Defining superBowlWin() function
+
+function superbowlWin(array) {
+  const found = array.find(item =>item.result === 'W');
+return found ? found.year : undefined;
+}
